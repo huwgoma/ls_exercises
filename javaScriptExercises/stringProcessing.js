0cd57139de5b3df6466a8630c73b6bcea7d3f57e
Hugo Ma <huwgoma@gmail.com>
2025-09-24 10:52:41 -0400

    js exercises string processing

--- javaScriptExercises/stringProcessing.js
+++ javaScriptExercises/stringProcessing.js
@@ -304,19 +304,21 @@ function highlightWord(searchTerm, text) {
   let words = text.split(' ');
 
   return words.map(word => {
-    return word.toLowerCase() === searchTerm.toLowerCase() ? 
+    let cleanWord = word.toLowerCase().replaceAll(/[^a-z]/g, '');
+
+    return cleanWord === searchTerm.toLowerCase() ? 
       `**${word.toUpperCase()}**` : word;
   }).join(' ');
 }
 
 // Regex Solution:
-// function highlightWord(searchTerm, text) {
-//   let pattern = new RegExp(`\\b${searchTerm}\\b`, 'ig');
+function highlightWord(searchTerm, text) {
+  let pattern = new RegExp(`\\b${searchTerm}\\b`, 'ig');
   
-//   return text.replaceAll(pattern, function(match) {
-//     return `**${match.toUpperCase()}**`;
-//   });
-// }
+  return text.replaceAll(pattern, function(match) {
+    return `**${match.toUpperCase()}**`;
+  });
+}
 
 // Example:
 const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
